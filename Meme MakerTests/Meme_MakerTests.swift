1572dd6c6faf65c41b766fbde70def00a4fa016f
avikantz <avikantsainidbz@gmail.com>
2016-04-04 09:47:28 +0530

    Initial Commit

---
+++ Meme MakerTests/Meme_MakerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Meme_MakerTests.swift
+//  Meme MakerTests
+//
+//  Created by Avikant Saini on 4/4/16.
+//  Copyright © 2016 avikantz. All rights reserved.
+//
+
+import XCTest
+@testable import Meme_Maker
+
+class Meme_MakerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
